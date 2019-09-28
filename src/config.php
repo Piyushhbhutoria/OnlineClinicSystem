d8a2625176a27c14c506d5bb514f4f5a3b2316cf
Piyushhbhutoria <piyush.bhutoria98@gmail.com>
2019-09-28 18:30:43 +0530

    config added

--- src/config.php
+++ src/config.php
@@ -1,2 +1,3 @@
 <?php
-$con = mysqli_connect("localhost","user","password", "database") or die(mysqli_error($con));
+// $con = mysqli_connect("localhost","user","password", "database") or die(mysqli_error($con));
+$con = mysqli_connect("localhost", "root", "empire123", "project") or die(mysqli_error($con));
